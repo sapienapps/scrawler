c2dcc364f74b0c07b0cec5e91373e87b04813c45
Jordan Burke <jordan.burke@gmail.com>
2014-11-26 15:04:11 -0500

    Changed the PageFetcher to use the connectionManager. Also fixed the client string.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "crawler4j"
 
-version := "3.5.6"
+version := "3.5.7"
 
 organization := "com.sapienapps"
 
--- src/main/java/edu/uci/ics/crawler4j/fetcher/PageFetcher.java
+++ src/main/java/edu/uci/ics/crawler4j/fetcher/PageFetcher.java
@@ -123,9 +123,9 @@ public class PageFetcher extends Configurable {
 
         httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(requestConfig)
-                .setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36")
+                .setConnectionManager(connectionManager)
+                .setUserAgent(config.userAgentString())
                 .addInterceptorLast(responseInterceptor).build();
-
 
         if (connectionMonitorThread == null) {
             connectionMonitorThread = new IdleConnectionMonitorThread(connectionManager);
